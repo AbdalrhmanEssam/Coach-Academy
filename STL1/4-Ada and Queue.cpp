9003b5bd4315fbfb1a180b220b57062def895eea
AbdalrhmanEssam <103788835+AbdalrhmanEssam@users.noreply.github.com>
2022-07-25 00:44:22 +0200

    Update 4-Ada and Queue.cpp

--- STL1/4-Ada and Queue.cpp
+++ STL1/4-Ada and Queue.cpp
@@ -1,28 +1,55 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<stack>
+#include<deque>
+#include<algorithm>
 using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    queue<int>q;
-    int t;
-    cin>>t;
-    while(t--){
-        int n,l;
-        cin>>n;
-        if(n==1){
-            cin>>l;
-            q.push(l);
+    deque<int>dq;
+    int q;
+    cin>>q;
+    bool flag=0;
+    while(q--){
+        string s;
+        cin>>s;
+        int val;
+        if(flag){
+            if(s=="toFront")
+                s="push_back";
+            else if(s=="push_back")
+                s="toFront";
+            else if(s=="back")
+                s="front";
+            else if(s=="front")
+                s="back";
         }
-        else if(n==2&&!q.empty()){
-            q.pop();
+        if(s=="toFront"){
+            cin>>val;
+            dq.push_front(val);
         }
-        else if(n==3){
-            if(q.empty())
-                cout<<"Empty!\n";
-            else cout<<q.front()<<'\n';
+        if(s=="push_back"){
+            cin>>val;
+            dq.push_back(val);
         }
+        else if(s=="back"){
+            if(!dq.empty()) {
+                cout << dq.back() << '\n';
+                dq.pop_back();
+            }
+            else cout<<"No job for Ada?\n";
+        }
+        else if(s=="front"){
+            if(!dq.empty()){
+                cout<<dq.front()<<'\n';
+                dq.pop_front();
+            }
+            else cout<<"No job for Ada?\n";
+        }
+        else if(s=="reverse")
+            flag=!flag;
     }
 }
